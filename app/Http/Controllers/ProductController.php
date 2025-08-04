e3df12f4d14126a588f5a7739335cd178dc2da4a
Hanny Carballo <hannycarballo@UDCC3-ESCANER.local>
2025-08-04 16:42:51 -0600

    Creando un nuevo controlador api

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all()->sortByDesc('id');
+        return view('products.index', compact('products'));
+    }
+
+    public function store(Request $request)
+    {
+        Product::create($request->all());
+        return redirect()->route('products.index');
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
