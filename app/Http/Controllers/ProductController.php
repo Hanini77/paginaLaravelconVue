febe4bf11115404dee8e2cba234a7e05a6fb89e2
Hanny Carballo <hannycarballo@UDCC3-ESCANER.local>
2025-08-04 18:23:42 -0600

    haciendo modificaciones

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 
+
 class ProductController extends Controller
 {
     public function index()
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/products', 'ProductController')->middleware('auth');
+//Route::get('/products', [ProductController::class, 'index']);
+Route::resource('/products', 'ProductController');
